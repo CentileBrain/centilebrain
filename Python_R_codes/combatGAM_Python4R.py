66695a455842c4190db70ffc26d333efa081c665
Ruiyang Ge <ruiyangge@hotmail.com>
2022-05-24 13:01:01 -0700

    Add files via upload

---
+++ Python_R_codes/combatGAM_Python4R.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from neuroHarmonize import harmonizationLearn
+import pandas as pd
+import numpy as np
+np.random.seed(0)
+
+def combatGAM_Python4R_function(path):
+    data = pd.read_csv(path + 'data_temp.csv')
+    my_data = np.array(data)
+    my_covars = pd.read_csv(path + 'covars_temp.csv')
+    my_model, my_data_adj = harmonizationLearn(my_data, my_covars, smooth_terms=['age'])
+    return my_data_adj, my_model
+
+
+# In[ ]:
+
+
+
+
