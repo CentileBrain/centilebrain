154293e04ada45cd2e7e57f23bf9e8fe74560315
Ruiyang Ge <ruiyangge@hotmail.com>
2024-03-15 12:08:57 -0700

    Update combatGAM_Python4R.py

--- Python_R_codes/combatGAM_Python4R.py
+++ Python_R_codes/combatGAM_Python4R.py
@@ -13,7 +13,7 @@ def combatGAM_Python4R_function(path):
     data = pd.read_csv(path + 'data_temp.csv')
     my_data = np.array(data)
     my_covars = pd.read_csv(path + 'covars_temp.csv')
-    my_model, my_data_adj = harmonizationLearn(my_data, my_covars, smooth_terms=['age'])
+    my_model, my_data_adj = harmonizationLearn(my_data, my_covars, smooth_terms=['age'], seed = 1)
     return my_data_adj, my_model
 
 
