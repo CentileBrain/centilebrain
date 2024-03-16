40ba168610f37b0ec9cc68949447c7384df1e10b
Ruiyang Ge <ruiyangge@hotmail.com>
2024-03-15 18:34:12 -0700

    Update combatGAM_Python4R.py

--- Python_R_codes/combatGAM_Python4R.py
+++ Python_R_codes/combatGAM_Python4R.py
@@ -3,8 +3,8 @@
 
 # In[1]:
 
-
-from neuroHarmonize import harmonizationLearn
+# preferable version of the neuroHarmonize, pandas, and numpy packages are 2.1.1, 2.2.1, and 1.26.4  
+from neuroHarmonize import harmonizationLearn 
 import pandas as pd
 import numpy as np
 np.random.seed(0)
@@ -13,7 +13,7 @@ def combatGAM_Python4R_function(path):
     data = pd.read_csv(path + 'data_temp.csv')
     my_data = np.array(data)
     my_covars = pd.read_csv(path + 'covars_temp.csv')
-    my_model, my_data_adj = harmonizationLearn(my_data, my_covars, smooth_terms=['age'], seed = 1)
+    my_model, my_data_adj = harmonizationLearn(my_data, my_covars, smooth_terms=['age'])
     return my_data_adj, my_model
 
 
